7287c7a057bef7c1153768a8a35d83d1d3e49250
Kylan Plunk <kaplunk1@memphis.edu>
2024-04-24 05:32:55 -0500

    eventfinder

---
+++ eventfinder.rb
@@ -0,0 +1,24 @@
+require 'dotenv/load'
+require 'httparty'
+require 'json'
+
+@api_key = ENV['EVENT_API_KEY']
+
+response = HTTParty.get("https://app.ticketmaster.com/discovery/v2/events?apikey=LwAi8LiwVjLATzg2SYJVCKChtooczqr4&locale=*&city=Memphis")
+event_data = JSON.parse(response.body)
+events = event_data['_embedded']['events']
+events.each do |event|
+  puts "Name: #{event['name']}"
+  venues = event['_embedded']['venues']
+  puts "Venues:"
+  venues.each do |venue|
+    puts venue['name']
+  end
+  puts "Date: #{event['dates']['start']['localDate']}"
+  puts "Time: #{event['dates']['start']['localTime']}"
+  puts "URL: #{event['url']}"
+  puts "-------------------"
+
+
+
+end
